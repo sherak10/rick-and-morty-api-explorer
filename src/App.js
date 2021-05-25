6738ca6c33623182887eb8e0e83852152ed987ca
simone.herak <simone.herak@axtesys.at>
2021-05-25 11:12:42 +0200

    Include NavBar and define routes in App; Add tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "components/pages/Home";
+import CharacterList from "components/pages/character/CharacterList";
+import EpisodeList from "components/pages/episode/EpisodeList";
+import LocationList from "components/pages/location/LocationList";
+import CharacterDetails from "components/pages/character/CharacterDetails";
+import EpisodeDetails from "components/pages/episode/EpisodeDetails";
+import LocationDetails from "components/pages/location/LocationDetails";
+import "./App.css";
+import NavBar from "components/pages/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="mt-2 mr-5 mb-3 ml-5">
+        <NavBar />
+
+        <Switch>
+          <Route path="/characters/:id">
+            <CharacterDetails />
+          </Route>
+
+          <Route path="/characters">
+            <CharacterList />
+          </Route>
+
+          <Route path="/episodes/:id">
+            <EpisodeDetails />
+          </Route>
+
+          <Route path="/episodes">
+            <EpisodeList />
+          </Route>
+
+          <Route path="/locations/:id">
+            <LocationDetails />
+          </Route>
+
+          <Route path="/locations">
+            <LocationList />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders characters link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // navigation links
+  expect(screen.getByText("Characters")).toBeInTheDocument();
+  expect(screen.getByText("Episodes")).toBeInTheDocument();
+  expect(screen.getByText("Locations")).toBeInTheDocument();
 });
